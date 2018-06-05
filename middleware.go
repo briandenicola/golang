911c2f0bb83367d7993f016a59dc9debadae8a1e
Brian Denicola <bjd145@yahoo.com>
2018-06-05 08:36:06 -0500

    Playing with build tasks

---
+++ middleware.go
@@ -0,0 +1,25 @@
+package main 
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func middlware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Executing middleware before request phase!")
+		handler.ServeHTTP(w, r)
+		fmt.Println("Executing middleware after request phase!")
+	})
+}
+	
+func mainLogic( w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Executing middleware .. . .")
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	mainLogicHandler := http.HandlerFunc(mainLogic)
+	http.Handle("/", middlware(mainLogicHandler))
+	http.ListenAndServe(":8000", nil)
+}
